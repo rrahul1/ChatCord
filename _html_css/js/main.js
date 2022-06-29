adb205a8c2714b681fb70282986d5f17ff6cf02e
rahul_rathod <rsr.encore@gmail.com>
2022-06-29 09:02:39 +0530

    new chatapp

---
+++ _html_css/js/main.js
@@ -0,0 +1,70 @@
+const chatForm = document.getElementById('chat-form');
+const chatMessage =  document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
+
+//get username and room from the link
+const { username, room } = Qs.parse(Location.search, {
+    ignoreQueryPrefix: true
+});
+
+const socket = io();
+
+// join chatroom
+socket.emit('joinRoom', {username, room});
+
+//get room and users
+socket.on('roomUsers', ({room, users}) => { 
+    outputRoomName(room);
+    outputUsers(users);
+});
+
+
+ 
+//message from server
+socket.on('message', message => {
+    console.log(message)
+    outputMessage(message);
+
+    //scroll down
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+//Message submit 
+chatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const msg = e.target.elements.msg.value;
+
+    // emit message to server
+    socket.emit('chatMessage',msg);
+
+    //clear input
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.focus(); 
+});
+
+//output message to DOM
+
+function outputMessage(message){
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `		<p class="meta">${message.user}name<span>$({message.time}</span></p>
+    <p class="text">
+        ${message.text}
+    </p>`;
+    document.querySelector('.chat-messages').appendChild(div);
+
+}
+
+//add room name to dom
+function outputRoomName(room){
+    roomName.innerText =  room;
+}
+
+//add users to dom
+function outputUsers(users) {
+    userList.innerHTML = `
+    ${users.map(user => `<li>{user.username}</li>`).join('')}
+    `;
+}
